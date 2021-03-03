737406737da10d6911475099803548552ad63050
Thomas Brown <thombrown@users.noreply.github.com>
2021-03-03 14:04:48 -0500

    Update v3_API_Example.ps1
    Updated to add a category to an existing VM

--- v3_API_Example.ps1
+++ v3_API_Example.ps1
@@ -12,11 +12,15 @@ add-type @"
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+#End of SSL Certificate snippet
+
 
 #Enter login info
 $clusterip = read-host "Enter Prism Central Cluster IP"
 $creds = Get-Credential
-$VMtoFind = "PrismCentral"
+$VMtoUpdate = "TomWin2019"
+$categoryName = "Environment"
+$categoryValue = "Dev"
 
 #Which endpoint you are trying to hit
 $Uri = "https://${clusterip}:9440/api/nutanix/v3/vms/list"
@@ -26,84 +30,28 @@ $body = @"
         
     }
 "@
+
 #Pass the credentials in the header
-$Header = @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($creds.UserName+":"+$creds.GetNetworkCredential().Password))}
-#Here is the actual API request. If using Powershell v6 or higher with a self signed certificate on Prism Central, use -SkipCertificateCheck on the end of the command
-$response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $Header -ContentType "application/json" -Body $body
+$Header = @{
+    "Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($creds.UserName+":"+$creds.GetNetworkCredential().Password))
+}
+
+#Here is the actual API request. If using Powershell v6 or higher with a self signed certificate on Prism Central, use -SkipCertificateCheck on the end of the command.  If using Powershell v5 or lower, remove -SkipCertificateCheck
+$response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $Header -ContentType "application/json" -Body $body -SkipCertificateCheck
 #Take the output and convert it from JSON to a readable format
 $VMs = (ConvertFrom-Json -InputObject $response.content).entities
+
 #Get the UUID of a specific VM
-$VMuuid = ($VMs | Where-Object {$_.spec.name -eq $VMtoFind}).metadata.uuid
-
-
-<#
-Create VM from a cloned image
-
-"metadata": {
-     "kind":"vm"
- },
- "spec": {
-     "name":"test",
-     "resources": {
-         "disk_list": [
-             {
-                 "data_source_reference": {
-                     "kind": "image",
-                     "uuid": "a91f56a0-2133-4080-b6b4-60961e94dad7"
-                 },
-                 "device_properties": {
-                     "device_type": "DISK",
-                     "disk_address": {
-                         "adapter_type": "SCSI",
-                         "device_index": 0
-                     }
-                 }
-             }
-         ],
-         "memory_size_mib": 1024,
-         "num_sockets": 1,
-         "num_vcpus_per_socket": 1,
-         "power_state": "OFF"
-     }
- }
-}
-
-#>
-
-
-<#
-Example of cloning from existing VM with sysprep
-
-{
-  "metadata":{
-     "kind":"vm"
-  },
-  "spec":{
-     "name":"VMCLONED001",
-     "cluster_reference":{
-        "kind":"cluster",
-        "name":"CLUSTERNUTA001",
-        "uuid":"00058128-10da-0XX-2c1f-98039b05d593"
-     },
-     "resources":{
-        "guest_customization":{
-           "is_overridable":false,
-           "sysprep":{
-              "install_type":"PREPARED",
-              "unattend_xml":"FNjaGVtYSgICm9(...)1cD5BZG1pbmlz="
-           }
-        },
-        "parent_reference":{
-           "kind":"vm",
-           "uuid":"b00cc669-cb2d-4235-aXXX-ff9b234bf8c9",
-           "name":"GoldenMaster001"
-        },
-        "power_state":"OFF"
-     }
-  }
-}
-
-
-#>
- 
- 
+$VMuuid = ($VMs | Where-Object {$_.spec.name -eq $VMtoUpdate}).metadata.uuid
+#Get the spec of that VM
+$spec = Invoke-WebRequest -Method Get -Uri "https://${clusterip}:9440/api/nutanix/v3/vms/$VMuuid" -Headers $Header -ContentType "application/json" -Body $body -SkipCertificateCheck
+#Take the output and convert it from JSON to a readable format
+$VMspec = (ConvertFrom-Json -InputObject $spec.Content)
+#Keep everything but the status key from the results
+$payload = $VMspec | Select-Object spec,api_version,metadata
+#Add a category to the VM. In this case I'm adding the default Environment:Dev category
+$payload.metadata.categories | add-member -name "$CategoryName" -value "$CategoryValue" -Membertype NoteProperty -Force
+#Convert the result back to JSON
+$JSONpayload = ConvertTo-Json -InputObject $payload -Depth 10
+#Send the updated VM specs back
+$spec = Invoke-WebRequest -Method Put -Uri "https://${clusterip}:9440/api/nutanix/v3/vms/$VMuuid" -Headers $Header -ContentType "application/json" -Body $JSONpayload -SkipCertificateCheck
